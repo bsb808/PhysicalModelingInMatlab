36015f900a8623c99e1cf1508d6f555a5ae0f50a
Brian Bingham <briansbingham@gmail.com>
2024-09-17 14:28:55 -0700

    Renaming chapters from numbers to words

---
+++ code/chap_vectors/create_vectors.m
@@ -0,0 +1,19 @@
+echo on;
+% Some ways to initialize vectors
+% As an list with step size = 1
+1:5
+% As a list with step size ~= 1
+1:3:10
+% As an N-by-1 column of zeros
+zeros(5,1)
+% As a 1-by-N row of ones
+ones(1,5)
+% A vector of psuedorandom numbers
+rand(1,5)
+% Manually, as a row vector
+[5 3.1 9.8 -2]
+% Manually as a column vector
+[5; 3.1; 9.8; -2;]
+% As a column using transpose
+[5 3.1 9.8 -2]'
+echo off;
